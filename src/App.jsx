f3308d246b7d75d579049806b6a3f36b3792efcb
OlegB83j <125024590+OlegB83j@users.noreply.github.com>
2026-01-23 09:33:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -185,7 +185,7 @@ function TrickyCube() {
 /** @type {{ EN: Translation, RU: Translation }} */
 const translations = {
   EN: {
-    heroTitle: 'MARS ATTACKS!',
+    heroTitle: 'MARS Fails!!!!!',
     heroSubtitle: 'THE INVASION HAS BEGUN • RESISTANCE IS FUTILE • WELCOME YOUR NEW OVERLORDS',
     join: 'JOIN THE INVASION',
     surrender: 'SURRENDER NOW',
